93111f0c14b630befcd5d41433e9719f9fca8628
Adriano <adrianocardoso1991@gmail.com>
2022-11-08 01:52:06 -0300

    Feat - Added folder to separate projects

---
+++ users_folders_permissions/iaac.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir publico
+mkdir adm
+mkdir ven
+mkdir sec
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+chmod 777 publico
+chmod 770 adm
+chown :GRP_ADM adm/
+chmod 770 ven
+chown :GRP_VEN ven/
+chmod 770 sec
+chown :GRP_SEC sec/
+
+useradd -g GRP_ADM -m -p $(openssl passwd password) -s /bin/bash carlos
+useradd -g GRP_ADM -m -p $(openssl passwd password) -s /bin/bash maria
+useradd -g GRP_ADM -m -p $(openssl passwd password) -s /bin/bash joao
+
+useradd -g GRP_VEN -m -p $(openssl passwd password) -s /bin/bash debora
+useradd -g GRP_VEN -m -p $(openssl passwd password) -s /bin/bash sebastiana
+useradd -g GRP_VEN -m -p $(openssl passwd password) -s /bin/bash roberto
+
+useradd -g GRP_SEC -m -p $(openssl passwd password) -s /bin/bash josefina
+useradd -g GRP_SEC -m -p $(openssl passwd password) -s /bin/bash amanda
+useradd -g GRP_SEC -m -p $(openssl passwd password) -s /bin/bash rogerio
